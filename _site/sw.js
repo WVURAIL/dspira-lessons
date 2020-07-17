5c68e6de6521412129f8acf1b1cbb23b928ee4e3
Pranav Sanghavi <prnv.snghv@gmail.com>
2020-07-17 16:25:29 -0400

    bigger update and reorganizations

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "saikiransripada-20200717161958";
+var CACHE_NAME = "saikiransripada-20200717162512";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/style.css?202007171619",
+        "/css/style.css?202007171625",
         "/"
       ]);
     })
---
+++ tags/Students/index.md
@@ -0,0 +1,4 @@
+---
+layout: center
+permalink: /students/
+---
