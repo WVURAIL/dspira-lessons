42b59aea3c19aaf11a2b8e76849245988eb40679
Pranav Sanghavi <prnv.snghv@gmail.com>
2020-07-17 16:44:03 -0400

    bigger update and reorganizations

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "saikiransripada-20200717163857";
+var CACHE_NAME = "saikiransripada-20200717164355";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/style.css?202007171638",
+        "/css/style.css?202007171643",
         "/"
       ]);
     })
--- tags/index.md
+++ tags/index.md
@@ -4,7 +4,8 @@ permalink: /tags/
 ---
 
 {% for tag in site.tags %}
-  <h3>{{ tag[0] }}</h3>
+  {% capture tag_name %}{{ tag[0] }}{% endcapture %}
+  <h3><a class="button" href="/tags/{{ tag_name }}">{{ tag_name }}</a></h3> 
   <ul>
     {% for post in tag[1] %}
       <li><a href="{{ post.url | relative_url }}">{{ post.title }}</a></li>
