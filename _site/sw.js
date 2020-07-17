990273a48ecc19f86a628d14a8ef081d162cfd0b
Pranav Sanghavi <prnv.snghv@gmail.com>
2020-07-17 13:07:59 -0400

    big update

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "saikiransripada-20200717121851";
+var CACHE_NAME = "saikiransripada-20200717130756";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/style.css?202007171218",
+        "/css/style.css?202007171307",
         "/"
       ]);
     })
