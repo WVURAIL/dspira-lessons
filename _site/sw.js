14f1741c982c47f612f5d800875732cf38a08a2b
Pranav Sanghavi <prnv.snghv@gmail.com>
2020-07-24 12:48:05 -0400

    ReOrg

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "saikiransripada-20200724124642";
+var CACHE_NAME = "saikiransripada-20200724124758";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/dspira-lessons/css/style.css?202007241246",
+        "/dspira-lessons/css/style.css?202007241247",
         "/dspira-lessons/"
       ]);
     })
