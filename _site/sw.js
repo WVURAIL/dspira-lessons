997aebf72363e125a4abbb973d6198d89f3c25bd
Pranav Sanghavi <prnv.snghv@gmail.com>
2020-08-20 16:16:12 -0400

    reorganization logos

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "saikiransripada-20200820161054";
+var CACHE_NAME = "saikiransripada-20200820161527";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/dspira-lessons/css/style.css?202008201610",
+        "/dspira-lessons/css/style.css?202008201615",
         "/dspira-lessons/"
       ]);
     })
